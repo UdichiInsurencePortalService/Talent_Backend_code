540ac9aaf6a1d6da7fe8a996b383030296272841
Diamour <diamourweb@gmail.com>
2026-01-29 17:11:53 +0530

    new changes

--- Model/postgressdb.js
+++ Model/postgressdb.js
@@ -1,43 +1,26 @@
 
-require("dotenv").config()
-const {Pool} = require("pg")
-const pool = new Pool({
-    host: process.env.DB_HOST,     // server
-  port: process.env.DB_PORT,     // default 5432
-  user: process.env.DB_USER,     // username
-  password: process.env.DB_PASSWORD, // password
-  database: process.env.DB_DATABASE, // database name
-})
 
-pool.connect()
-.then(()=>console.log("📌 PostgreSQL Connected Successfully"))
-.catch(err =>console.log("❌ PostgreSQL Connection Error:", err.message));
+const { Pool } = require("pg");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+pool
+  .connect()
+  .then(() => console.log("✅ PostgreSQL Connected Successfully (Neon)"))
+  .catch((err) =>
+    console.error("❌ PostgreSQL Connection Error:", err.message)
+  );
 
 module.exports = pool;
-
-
-
-// const { Pool } = require("pg");
-
-// if (process.env.NODE_ENV !== "production") {
-//   require("dotenv").config();
-// }
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
-// pool
-//   .connect()
-//   .then(() => console.log("✅ PostgreSQL Connected Successfully (Neon)"))
-//   .catch((err) =>
-//     console.error("❌ PostgreSQL Connection Error:", err.message)
-//   );
-
-// module.exports = pool;
 
 
 
